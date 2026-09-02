c0724294246a44161f79bc9dd5ba4871d807d171
totalwarehousepaita-cloud <totalwarehouse.paita@gmail.com>
2026-09-02 12:07:24 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // Pega aquí la Project URL y la anon public key.
 // La anon key es una clave pública pensada para usarse en el navegador.
 window.LA_DOMINGA_CONFIG = {
-  SUPABASE_URL: "https://ysnxncbyhtjeeymnklks.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://ysnxncbyhtjeeymnklks.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_z8zrpUbNp27isZNqZ6yGWQ_DKD_45mp"
 };
